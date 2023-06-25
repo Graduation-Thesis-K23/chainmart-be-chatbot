6538b33bacf855f0f48fb830c5ec04debd46b1ad
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-25 18:44:41 +0700

    update

--- actions/actions.py
+++ actions/actions.py
@@ -3,19 +3,42 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHelloWorld(Action):
-    def name(self) -> Text:
-        return "action_mood_great"
+products = [
+    {
+        "name": "Nấm bào ngư trắng 300g",
+        "price": 14.900,
+        "url": "https://www.bachhoaxanh.com/nam-tuoi/nam-bao-ngu-trang-bich-300g",
+    },
+    {
+        "name": "10 gói mì 3 Miền Gold bò hầm rau thơm 75g",
+        "price": 50.000,
+        "url": "https://www.bachhoaxanh.com/mi/10-goi-mi-3-mien-gold-bo-ham-rau-thom-goi-75g",
+    },
+    {
+        "name": "6 chai nước ngọt Coca Cola 390ml",
+        "price": 60.000,
+        "url": "https://www.bachhoaxanh.com/nuoc-ngot/6-chai-nuoc-ngot-coca-cola-390ml",
+    },
+]
 
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
+orders = [
+    {
+        "id": "ORDER001",
+        "status": "Đang vận chuyển",
+    },
+    {
+        "id": "ORDER002",
+        "status": "Đã giao hàng",
+    },
+    {
+        "id": "ORDER003",
+        "status": "Đã hủy",
+    },
+    {
+        "id": "ORDER004",
+        "status": "Đã giao hàng",
+    },
+]
 
 
 class ActionProductDetails(Action):

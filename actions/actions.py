3e8cb03dc168911581752306e2abf7adf18b6fa3
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-27 14:52:21 +0700

    feat: new name

--- actions/actions.py
+++ actions/actions.py
@@ -113,26 +113,3 @@ class CungCapTenSanPham(Action):
             )
 
         return []
-
-
-class ActionProductDetails(Action):
-    def name(self) -> Text:
-        return "action_tra_cuu_trang_thai_don_hang"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-        # get so_don_hang
-        so_don_hang = tracker.get_slot("so_don_hang")
-        sender_id = tracker.sender_id
-
-        dispatcher.utter_message(
-            text=f"Đơn hàng {so_don_hang} của {sender_id} đang được vận chuyển.".format(
-                so_don_hang, sender_id
-            )
-        )
-
-        return []
